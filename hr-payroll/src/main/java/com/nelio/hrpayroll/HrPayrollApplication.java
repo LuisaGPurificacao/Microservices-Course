1f35b1d776295d427d9ebb7ccd43574b3584f3cc
luisa-purificacao <luisa.purificacao@ibm.com>
2023-03-30 15:42:41 -0300

    Feign Client

--- hr-payroll/src/main/java/com/nelio/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/nelio/hrpayroll/HrPayrollApplication.java
@@ -1,13 +1,15 @@
 package com.nelio.hrpayroll;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayrollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrPayrollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
 
 }
