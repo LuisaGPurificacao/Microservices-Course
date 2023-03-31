bc844e95bb33ad43eeb27202bb23065a5f4e893a
luisa-purificacao <luisa.purificacao@ibm.com>
2023-03-31 09:37:37 -0300

    ribbon balancing

--- hr-payroll/src/main/java/com/nelio/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/nelio/hrpayroll/HrPayrollApplication.java
@@ -1,9 +1,11 @@
 package com.nelio.hrpayroll;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RibbonClient(name = "hr-worker")
 @SpringBootApplication
 @EnableFeignClients
 public class HrPayrollApplication {
